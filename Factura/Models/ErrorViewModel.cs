6f42461b08b3c2909fd69056b99a780ba722615d
usuario <usuario@usuarios-MacBook-Pro.local>
2021-09-17 00:51:55 -0500

    Factura

---
+++ Factura/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Factura.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
