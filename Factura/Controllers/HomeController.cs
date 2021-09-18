4c4587ded6a74e00a09719494ad4649733c12603
usuario <usuario@usuarios-MacBook-Pro.local>
2021-09-18 00:23:54 -0500

    Creacion Factura

--- Factura/Controllers/HomeController.cs
+++ Factura/Controllers/HomeController.cs
@@ -28,6 +28,55 @@ namespace Factura.Controllers
             return View();
         }
 
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Factura()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Productos()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Clientes()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult EditarCliente()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult EliminarCliente()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
